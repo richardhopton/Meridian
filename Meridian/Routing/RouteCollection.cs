37222f78d7fefc8feffbf889317954a1f7e11177
SND\irtimaled_cp <SND\irtimaled_cp@142e9c35-602f-4880-9f37-ec48a24daa6a>
2009-11-12 21:11:57 +0000

    Move map route methods to RouteCollection class from RouteCollectionExtensions.
    git-svn-id: https://meridian.svn.codeplex.com/svn/Trunk@39964 142e9c35-602f-4880-9f37-ec48a24daa6a

--- Meridian/Routing/RouteCollection.cs
+++ Meridian/Routing/RouteCollection.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq.Expressions;
 
 namespace Meridian.Routing
 {
@@ -7,6 +9,67 @@ namespace Meridian.Routing
     {
         private readonly Dictionary<string, RouteBase> _namedMap = new Dictionary<string, RouteBase>();
 
+        private static String GetFuncParameter(Expression<Func<String, String>> func)
+        {
+            if (func != null)
+            {
+                return func.Parameters[0].Name;
+            }
+            return null;
+        }
+
+        private static String GetFuncResult(Expression<Func<String, String>> func)
+        {
+            if (func != null)
+            {
+                if (func.Body is ConstantExpression)
+                {
+                    return (String)(func.Body as ConstantExpression).Value;
+                }
+                return func.Compile().Invoke(null);
+            }
+            return null;
+        }
+
+        public RouteBase MapRoute(string url)
+        {
+            return MapRoute(null /* name */, url, null /* defaults */);
+        }
+
+        public RouteBase MapRoute(string name, string url)
+        {
+            return MapRoute(name, url, null /* defaults */);
+        }
+
+        public RouteBase MapRoute(string url, params Expression<Func<String, String>>[] defaults)
+        {
+            return MapRoute(null /* name */, url, defaults);
+        }
+
+        public RouteBase MapRoute(string name, string url, params Expression<Func<String, String>>[] defaults)
+        {
+            Requires.NotNullOrEmpty(url, "url");
+
+            RouteValueDictionary routeValueDefaults = null;
+            if (defaults != null)
+            {
+                routeValueDefaults = new RouteValueDictionary();
+                foreach (var expr in defaults)
+                {
+                    routeValueDefaults.Add(GetFuncParameter(expr), GetFuncResult(expr));
+                }
+            }
+
+            var route = new Route
+            {
+                Url = url,
+                Defaults = routeValueDefaults
+            };
+            Add(name, route);
+
+            return route;
+        }
+        
         public void Add(string name, RouteBase route)
         {
             Requires.NotNull(route, "route");
