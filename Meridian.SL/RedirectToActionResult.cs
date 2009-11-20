c169acb7a0f656ed67a5c21a3d6a1db5987b6efc
SND\gsobocinski_cp <SND\gsobocinski_cp@142e9c35-602f-4880-9f37-ec48a24daa6a>
2009-11-20 15:33:32 +0000

    Added RedirectToActionResult, will need to think about what we do when redirecting with multiple frames supported.
    git-svn-id: https://meridian.svn.codeplex.com/svn/Trunk@41551 142e9c35-602f-4880-9f37-ec48a24daa6a

---
+++ Meridian.SL/RedirectToActionResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Meridian.SL
+{
+    public class RedirectToActionResult : IActionResult
+    {
+        public string Url { get; set; }
+
+        public void Execute(ActionContext context)
+        {
+            //Possibly need to think about how we handle a redirect to action with multiple frames
+            Navigation.NavigationService.Default().Navigate(Url);
+        }
+    }
+}
--- Meridian/ViewResult.cs
+++ Meridian/ViewResult.cs
@@ -10,14 +10,15 @@ namespace Meridian
         public void Execute(ActionContext context)
         {
             Requires.NotNull(context, "context");
+
             if(String.IsNullOrEmpty(ViewName))
             {
                 ViewName = context.ActionName;
             }
-            IView view = ViewEngineManager.CurrentEngine.GetView(ViewName);
+            var view = ViewEngineManager.CurrentEngine.GetView(ViewName);
             if (view != null)
             {
-                ViewContext viewContext = new ViewContext(context.ControllerContext, ViewData);                
+                var viewContext = new ViewContext(context.ControllerContext, ViewData);                
                 view.Render(viewContext);
             }
         }
